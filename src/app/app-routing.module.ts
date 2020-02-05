856aeb95e036133681454ba72763ed59ab177eff
George Pears <george@pears.me.uk>
2020-02-05 13:16:48 +0000

    Added dashboard and runner pages to git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {RunnersComponent} from './runners/runners.component';
 import {SignInComponent} from './account/sign-in/sign-in.component';
+import {AngularFireAuthGuard, canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['sign-in']);
+const redirectLoggedInToItems = () => redirectLoggedInTo(['runners']);
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: DashboardComponent },
-  { path: 'runners', component: RunnersComponent },
-  { path: 'sign-in', component: SignInComponent },
+  { path: 'runners', component: RunnersComponent},
+  { path: 'sign-in', component: SignInComponent},
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAwbjIZk3bV_900Xi7mvcVv8hzKlFBP7zo',
+    authDomain: 'wide-game.firebaseapp.com',
+    databaseURL: 'https://wide-game.firebaseio.com',
+    projectId: 'wide-game',
+    storageBucket: 'wide-game.appspot.com',
+    messagingSenderId: '459505349782',
+    appId: '1:459505349782:web:3b3ca7ce2d795fcafbbeb7',
+    measurementId: 'G-4S3JJFHKRN'
+  }
 };
 
 /*
