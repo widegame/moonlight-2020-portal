cfa4c2eed59157e5788e9b4034eb1fd652bf6992
George Pears <george@pears.me.uk>
2020-02-06 14:04:56 +0000

    Added firestore to runners.component.ts

--- src/app/runners/runners.component.ts
+++ src/app/runners/runners.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-runners',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RunnersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
   }
